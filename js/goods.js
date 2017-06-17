6a3823087ef15f1d2a89770ef194c3e560ccdb0f
WaterCube <water471871679@gmail.com>
2017-06-17 14:10:57 +0800

    实现点击按钮异步更新页面排版

--- js/goods.js
+++ js/goods.js
@@ -1,9 +1,110 @@
 
-console.log(2);
+var page = 1;
+    //$(".navi li a").click(function(){
+    //    $(this).addClass("selected").siblings().removeClass("selected");
+    //})
+$(function(){
+    var shopPath = 'http://friend.vipspace.top:8080/space-shop-web-1.0-SNAPSHOT';//商城
+    var shopOther1Path = 'http://192.168.20.242:8086';//商城卖家提现
 
-    $("button").click(function(){
-        console.log("click")
+    var shopOtherPath = 'http://192.168.40.160:8080/space-account-web-1.0-SNAPSHOT';//账号  测试接口
+    var spacePath = 'http://customer.vipspace.top:8083/collect-console-1.0-SNAPSHOT';//空间站
+    var imagePath = 'http://friend.vipspace.top:8080/ar-console-1.0-SNAPSHOT';//图片
+    var spaceMusicPath = 'http://customer.vipspace.top:8083/net-cache-helper-console-1.0-SNAPSHOT';//空间站音乐
+    var spaceMusicSearchPath = 'http://customer.vipspace.top:8083/space-search-history-console-1.0-SNAPSHOT';//空间站音乐搜索
+
+
+    var accountPath = shopOtherPath ;//账号
+    var loginSpaceid = 0;//37226346681
+    var tokenid = '';
+    var spaceid=37226346680;
+
+//读取cookies
+    function getCookie(name)
+    {
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+        if(arr=document.cookie.match(reg))
+            return (arr[2]);
+        else
+            return null;
+    }
+// console.log(getCookie('loginspaceid'));
+    loginSpaceid = getCookie('loginspaceid');
+    tokenid = getCookie('tokenid');
+    var spaceid = loginSpaceid;//
+
+var store={
+    init:function(){
+        this.changeNav();
+        this.changeStyle();
+    },
+    //点击导航
+    changeNav:function(){
+         $(".navi li a").each(function(){
+             $(this).click(function(){
+                 $(".navi li a").removeClass("selected")
+                 $(this).addClass("selected");
+             })
+         })
+     },
+    //版面更改样式
+    changeStyle:function(){
+        var float=true;
+        $(".checklist_icon").click(function(){
+            if(float==true){
+                $(".checklist_icon").attr('src','img/checklist-a@2x.png');
+                $(".goods").attr('id','goods_col');
+                float=false;
+            }else{
+                $(".checklist_icon").attr('src','img/checklist@2x.png');
+                $(".goods").attr('id','goods_row');
+                float=true;
+            }
+        })
+},
+};
+
+    store.init();
+
+    $.ajax({
+        url: shopPath+"/shop/prd/b/findPrdList",
+        dataType:'json',
+        data:{
+            spaceid:37226346680,
+            pageNumber:page++,
+            pageSize:10
+        },
+        success:function(res){
+            console.log(res);
+            var result=res.data;
+            var div='';
+            for(var i=0;i<result.length;i++){
+                div+='<div class="goods" id="goods_row">'+
+                    '<div class="goods_img">'+'<img src=" '+result[i].coverImgurl+'" alt="">'+'</div>'+
+                    '<div class="goods_details_box1">'+
+                    '<div class="product_details">'+
+                    '<p class="product_description">'+
+                    result[i].productName+
+                    '</p>'+
+                    ' <p class="product_adds">'+ result[i].deliverAddress+
+                    '</p>'+
+                    '<img src="img/baoyou.png" class="poster">'+
+                    '</img>'+
+                    '<span class="goods_price_icon">￥</span>'+
+                    '<span class="goods_price">'+result[i].salePrice +
+                    '</span>'+'<span class="people">'+
+                    result[i].salesVolume+
+                    '</span>'+
+                    '<span class="pay">付款</span>'+
+                    '<span class="cart"></span>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>'
+            }
+            $(".products").append(div);
+
+        },
+        error: function (XMLHttpRequest, textStatus, thrownError) {
+        }
     })
-
-
-
+})
